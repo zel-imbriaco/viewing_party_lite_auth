c39404b75543e2b22ebcd4fbac9d8f321de07d71
Zachary Imbriaco <zachary@imbriaco.com>
2022-05-23 14:41:58 -0400

    Test: New sad path test for registration.

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe 'New User Page', type: :feature do
     new_user = User.where(email: 'lorem@ipsum.dorum').first
     expect(current_path).to eq("/users/#{new_user.id}")
   end
+
+  describe 'sad path' do
+    before :each do
+      visit '/users/new'
+    end
+    it 'requires password and password_confirmation to be identical' do
+      within '#new-user-form' do
+        fill_in :email, with: 'lorem@ipsum.dorum'
+        fill_in :password, with: 'test'
+        fill_in :password_confirmation, with: 'test'
+        click_on :submit
+      end
+
+      expect(current_path).to eq('/users/new')
+      expect(flash[:alert]).to be_present
+    end
+  end
 end
