30c2b6ac67c6452a91a77afdbb64818b1d5e7c8b
Zachary Imbriaco <zachary@imbriaco.com>
2022-05-24 17:13:36 -0400

    Refactor: 'as a user' describe block on root path.

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Landing/Welcome Page' do 
 
   describe 'As a Visitor' do
-    before do 
-      @skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password: 'test')
-      @lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com', password: 'test')
-      @hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com', password: 'test')
-    end 
+
 
     it 'has the title of the application, and a link to login' do
       
@@ -27,14 +23,14 @@ RSpec.describe 'Landing/Welcome Page' do
       expect(current_path).to eq login_path
       
     end 
+    
 
-    it 'cannot see user list if not logged in' do
+    it 'cannot see user list if a visitor' do
       visit root_path
       
       expect(page).to_not have_content("#existing_users")
     end
 
-  
     it 'has a link to return back to the landing/welcome page' do #link will be present on every page of application
       
       visit "/"
@@ -52,4 +48,27 @@ RSpec.describe 'Landing/Welcome Page' do
       expect(current_path).to eq('/users/new')
     end 
   end 
+  describe 'As a user' do
+    before :each do
+      @skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password: 'test')
+      @lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com', password: 'test')
+      @hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com', password: 'test')
+      
+      visit login_path
+      fill_in :email, with: @skeeter.email
+      fill_in :password, with: @skeeter.password
+      click_on :submit    
+    end
+
+    it 'links to log out a user if logged in' do
+
+      visit root_path
+
+      expect(page).to have_link 'Log Out'
+      click_on 'Log Out'
+
+      expect(current_path).to eq '/'
+      expect(page).to have_link 'Login'
+    end    
+  end
 end 
